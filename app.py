4f5473533b70066dbcd98fbe7c35f5a08970334f
j272 <j272@pjwstk.edu.pl>
2025-08-12 16:58:46 +0200

    Utworzenie bazy danych

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from pydantic import BaseModel
+
+import sqlite3
 
 app = FastAPI()
+
+
+conn = sqlite3.connect("travel.db", check_same_thread=False)
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS trips (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        destination TEXT NOT NULL,
+        month TEXT NOT NULL,
+        price_pln REAL
+    )
+    """)
+conn.commit()
+conn.close()
+
+def add_trip(destination: str, month: str, price: float):
+    cur.execute("INSERT INTO trips (destination, month, price) VALUES (?, ?, ?)",("Greece", "May", 655))
+    conn.commit()
+    return
+
+def read_trips():
+    cur.execute("SELECT * FROM trips")
+    return cur.fetchall()
+
+def read_trip(destination: str):
+    cur.execute("SELECT * FROM trips WHERE destination = ?", (destination,))
+    return cur.fetchone()
+
+
+
+
+
 
 @app.get("/health")
 def read_health():
