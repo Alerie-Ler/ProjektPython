22f9bcfb8fb6eedf7122e7ffc106797dcf06baa0
j270 <j270@pjwstk.edu.pl>
2025-08-12 16:57:19 +0200

    class Trip

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field, validator
+from typing import Dict
 
 app = FastAPI()
+
+class Trip(BaseModel):
+    destination: str
+    month: str
+    price_pln: float
+    @validator('destination', 'month')
+    def check_not_empty(cls, v):
+        if not v or v.strip() == "":
+            raise ValueError('Pole nie może być puste')
+        return v
+    @validator('price_pln')
+    def check_price(cls, v):
+        if v < 0:
+            raise ValueError('Cena musi byc większa lub równa 0')
+        return v
+
 
 @app.get("/health")
 def read_health():
     return {"status": "ok"}
+
